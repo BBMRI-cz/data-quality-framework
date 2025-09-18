0869030f78e8202567b14baef40aa1ee9de2e124
Radovan Tomasik <tomasik.radovan@gmail.com>
2025-09-18 16:03:54 +0200

    Add HTTP Request logging filter

--- modules/backend/src/main/java/eu/bbmri_eric/quality/agent/common/SpaController.java
+++ modules/backend/src/main/java/eu/bbmri_eric/quality/agent/common/SpaController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class SpaController {
   @RequestMapping(value = "/{path:[^.]*}", method = RequestMethod.GET)
   public String redirect() {
-    // Forward to index.html for SPA routes
     return "forward:/index.html";
   }
 }
