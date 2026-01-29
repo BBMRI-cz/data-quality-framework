74a1abc595468ff4e8f1ac7c6d74e3bd91ef1b79
Radovan Tomasik <tomasik.radovan@gmail.com>
2026-01-29 10:00:12 +0100

    fix(agent): remove old timeliness check

--- agent/backend/src/main/resources/db/migration/V1.1__add_total_samples_to_report.sql
+++ agent/backend/src/main/resources/db/migration/V1.1__add_total_samples_to_report.sql
@@ -1,5 +1,7 @@
 ALTER TABLE report ADD COLUMN number_of_secondary_entities INTEGER ;
 
+DELETE FROM cql_check WHERE name = 'Last update happened more than a year ago';
+
 INSERT INTO cql_check (name, description, query, warning_threshold, error_threshold)
 VALUES (
            'Patients without specimen',
