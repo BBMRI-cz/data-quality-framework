bdeb6a4335c34672456a58aebb6c963089a3585d
Radovan Tomasik <tomasik.radovan@gmail.com>
2026-01-29 09:12:54 +0100

    feat(agent): add check for patients without samples

--- agent/backend/src/main/resources/db/migration/V1.1__add_total_samples_to_report.sql
+++ agent/backend/src/main/resources/db/migration/V1.1__add_total_samples_to_report.sql
@@ -1,1 +1,23 @@
 ALTER TABLE report ADD COLUMN number_of_secondary_entities INTEGER ;
+
+INSERT INTO cql_check (name, description, query, warning_threshold, error_threshold)
+VALUES (
+           'Patients without specimen',
+           'How many patients do not have any specimens',
+           'library PatientWithoutSpecimen version ''1.0.0''
+using FHIR version ''4.0.0''
+include FHIRHelpers version ''4.0.0''
+
+context Patient
+
+define HasNoSpecimen:
+  not exists (
+    [Specimen] S
+      where S.subject.reference = ''Patient/'' + Patient.id
+  )
+
+define InInitialPopulation:
+  HasNoSpecimen',
+           10, 30
+       );
+
