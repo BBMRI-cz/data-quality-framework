5208979df48e12e01a93d7d43d5dddecd4e9b3c4
RadovanTomik <tomasik.radovan@gmail.com>
2026-02-14 19:28:15 +0100

    refactor(agent): replace EventListener with TransactionalEventListener and add event_publication table

--- agent/backend/src/main/java/eu/bbmri_eric/quality/agent/dataquality/listener/ReportGeneratingListener.java
+++ agent/backend/src/main/java/eu/bbmri_eric/quality/agent/dataquality/listener/ReportGeneratingListener.java
@@ -3,9 +3,9 @@ package eu.bbmri_eric.quality.agent.dataquality.listener;
 import eu.bbmri_eric.quality.agent.dataquality.ReportPipeline;
 import eu.bbmri_eric.quality.agent.dataquality.event.NewReportEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -18,7 +18,7 @@ class ReportGeneratingListener {
   }
 
   @Async
-  @EventListener
+  @TransactionalEventListener
   public void onNewReportEvent(NewReportEvent event) {
     log.info("Received new report event for report id: {}", event.getReportId());
     reportPipeline.run(event.getReportId());
