6e24144dc0fd175cbb8f159d80aec9a089164d0a
Ivan Mahut <ivan.mahut.92@gmail.com>
2026-03-09 14:08:48 +0100

    feat(server): make NoiseMechanism enum public for differential privacy settings

--- agent/backend/src/main/java/eu/bbmri_eric/quality/agent/settings/NoiseMechanism.java
+++ agent/backend/src/main/java/eu/bbmri_eric/quality/agent/settings/NoiseMechanism.java
@@ -1,4 +1,4 @@
-package eu.bbmri_eric.quality.agent.settings.domain;
+package eu.bbmri_eric.quality.agent.settings;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
