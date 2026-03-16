32a82e4c152155d9f3dcd7656c3e34a8a88127ef
Ivan Mahut <ivan.mahut.92@gmail.com>
2026-03-16 15:26:20 +0100

    feat(agent): implement default values for differential privacy settings

--- agent/backend/src/main/java/eu/bbmri_eric/quality/agent/dataquality/impl/DifferentialPrivacyConfig.java
+++ agent/backend/src/main/java/eu/bbmri_eric/quality/agent/dataquality/impl/DifferentialPrivacyConfig.java
@@ -1,6 +1,7 @@
 package eu.bbmri_eric.quality.agent.dataquality.impl;
 
 import eu.bbmri_eric.quality.agent.settings.NoiseMechanism;
+import eu.bbmri_eric.quality.agent.settings.dto.SettingsDTO;
 import eu.bbmri_eric.quality.agent.settings.event.SettingsUpdatedEvent;
 import lombok.Getter;
 import org.springframework.context.event.EventListener;
@@ -8,7 +9,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Getter
-public class DifferentialPrivacyConfig {
+class DifferentialPrivacyConfig {
+  private static final double DEFAULT_EPSILON = 3.0;
+  private static final double DEFAULT_DELTA = 1.0E-8;
+  private static final int DEFAULT_MIN_THRESHOLD = 50;
+  private static final NoiseMechanism DEFAULT_NOISE_MECHANISM = NoiseMechanism.GAUSSIAN;
+
   private double epsilon;
   private double delta;
   private int minThreshold;
@@ -18,9 +24,16 @@ public class DifferentialPrivacyConfig {
 
   @EventListener
   private void updateConfig(SettingsUpdatedEvent event) {
-    this.epsilon = event.getSettings().getEpsilon();
-    this.delta = event.getSettings().getDelta();
-    this.minThreshold = event.getSettings().getMinThreshold();
-    this.noiseMechanism = event.getSettings().getNoiseMechanism();
+    SettingsDTO settings = event.getSettings();
+    this.epsilon = settings.getEpsilon() != null ? settings.getEpsilon() : DEFAULT_EPSILON;
+    this.delta = settings.getDelta() != null ? settings.getDelta() : DEFAULT_DELTA;
+    this.minThreshold =
+        settings.getMinThreshold() != null
+            ? settings.getMinThreshold()
+            : DEFAULT_MIN_THRESHOLD;
+    this.noiseMechanism =
+        settings.getNoiseMechanism() != null
+            ? settings.getNoiseMechanism()
+            : DEFAULT_NOISE_MECHANISM;
   }
 }
