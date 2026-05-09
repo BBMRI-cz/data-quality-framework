f59ebac88205b1dfcaf6918bd0ac7c685955fb64
Radovan Tomasik <tomasik.radovan@gmail.com>
2026-05-09 20:41:38 +0200

    test: mark ServerControllerTest with DirtiesContext to reset context after each test

--- agent/backend/src/test/java/eu/bbmri_eric/quality/agent/server/impl/ServerControllerTest.java
+++ agent/backend/src/test/java/eu/bbmri_eric/quality/agent/server/impl/ServerControllerTest.java
@@ -14,10 +14,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ServerControllerTest {
 
   private static final String SERVERS_ENDPOINT = "/api/servers";
