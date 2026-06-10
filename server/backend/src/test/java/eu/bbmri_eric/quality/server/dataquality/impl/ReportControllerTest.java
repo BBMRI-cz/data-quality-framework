c0acdf673b9e99d2e86ab70cf40ee87899861575
Radovan Tomasik <tomasik.radovan@gmail.com>
2026-06-10 11:59:43 +0200

    fix(server): fix failing tests

--- server/backend/src/test/java/eu/bbmri_eric/quality/server/dataquality/impl/ReportControllerTest.java
+++ server/backend/src/test/java/eu/bbmri_eric/quality/server/dataquality/impl/ReportControllerTest.java
@@ -13,6 +13,7 @@ import eu.bbmri_eric.quality.server.dataquality.domain.QualityCheck;
 import eu.bbmri_eric.quality.server.dataquality.domain.Report;
 import eu.bbmri_eric.quality.server.dataquality.dto.QualityCheckResultDTO;
 import eu.bbmri_eric.quality.server.dataquality.dto.ReportCreateRequest;
+import eu.bbmri_eric.quality.server.util.IntegrationTest;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -21,18 +22,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
+@IntegrationTest
 @Transactional
 class ReportControllerTest {
 
